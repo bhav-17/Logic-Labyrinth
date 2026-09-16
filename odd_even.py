b3986385c104cd483813a0afad79081b0334b675
bhav-17 <bhavya.prakash.cg@gmail.com>
2026-09-16 13:01:26 +0530

    updated solution

--- odd_even.py
+++ odd_even.py
@@ -1,5 +1,7 @@
 #Q. Given a number n, check whether it is even or odd. Return true for even and false for odd.
 
+#Solution:
+
 num=int(input("Enter a number: "))
 def check(num):
     if num % 2 == 0:
