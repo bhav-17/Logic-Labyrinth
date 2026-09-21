15a1a465b3beb869110f5ac599829898219e81f1
bhav-17 <bhavya.prakash.cg@gmail.com>
2026-09-21 23:46:08 +0530

    updated solution

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -1,18 +1,12 @@
 #Q. Given a number n, find the sum of its digits.
+
 #Solution:
 
 num=input("Enter your number: ")
 sum=0
 for i in num:
-     sum+=int(i)
-if sum<10:
-   print(sum)
-elif sum>=10:
-   new_num=str(sum)
-   sum=0
-   for j in new_num:
-       sum+=int(j)
-   print(sum)
+   sum+=int(i)
+print(sum)
        
 
 
