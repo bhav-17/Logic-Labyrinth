fce73812197e852be4d40a4f00afb9bed48ce974
bhav-17 <bhavya.prakash.cg@gmail.com>
2026-09-22 22:08:58 +0530

    odd even analysis

---
+++ odd_even_analyzer.py
@@ -0,0 +1,15 @@
+num_list =input("Enter numbers separated by comma: ").split(",")
+even_list=[]
+odd_list=[]
+even_count=0
+odd_count=0
+for i in num_list:
+    n_i=int(i)
+    if n_i%2==0:
+        even_list.append(n_i)
+        even_count+=1
+    else:
+        odd_list.append(n_i)
+        odd_count+=1
+print(f"Even numbers= {even_list} count= {even_count}")
+print(f"Odd numbers= {odd_list} count= {odd_count}")
