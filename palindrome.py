54cea90ba1cff6b05f85ce5354c2aa73b09cb620
bhav-17 <bhavya.prakash.cg@gmail.com>
2026-09-24 14:15:56 +0530

    updated question

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,7 @@
+#Given an integer n, determine whether it is a palindrome number or not. A number is called a palindrome if it reads the same from forward and backward.
+
+# Solution:
+
 num=int(input('Enter a number: '))
 rev_num=int(str(num)[::-1])
 if num==rev_num:
